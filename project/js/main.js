ae98789eb09328ee01ce876fd8a0e858d96ea523
carvillain <owen.carvill@gmail.com>
2021-09-11 23:54:28 -0500

    Weather App

---
+++ project/js/main.js
@@ -0,0 +1,33 @@
+let apiKey = "949b92bbd47bb1f5763bafe887da8df1";
+
+fetchData = function (city) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    ).then((response) => response.json())
+    .then((data) => displayWeather(data));
+};
+
+displayWeather = function (data) {
+    const city_name = data.name;
+    const current_temp = Math.round(data.main.temp);
+    const temp_high = Math.round(data.main.temp_max);
+    const temp_low = Math.round(data.main.temp_min);
+    const humidity = Math.round(data.main.humidity);
+    const forecast = data.weather[0].description;
+    document.querySelector('.city').innerText = `${city_name}'s Weather: `
+    document.querySelector('.current').innerText = `Current Temperature: ${current_temp}°F`
+    document.querySelector('.temp_high').innerText = `High Temperature: ${temp_high}°F`
+    document.querySelector('.temp_low').innerText = `Low Temperature: ${temp_low}°F`
+    document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`
+    document.querySelector('.forecast').innerText = `Forecast: ${forecast}`
+}
+
+search = function (){
+    fetchData(document.querySelector('.search').value);
+}
+
+document.querySelector('.input-section button').addEventListener('click', function() {
+    search();
+})
+
+
+fetchData('Chicago')
